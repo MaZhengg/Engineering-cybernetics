e2ec70089a72d5f67d41e168296ab60ebbde6294
MaZhengg <ma_zhg@qq.com>
2025-12-05 11:27:13 +0800

    初始提交
    Signed-off-by: MaZhengg <ma_zhg@qq.com>

---
+++ 删除中间文件.bat
@@ -0,0 +1,14 @@
+@echo off
+
+del *.aux
+del nkthesis.bib.bak
+del Engineering cybernetics.bbl
+del Engineering cybernetics.bcf
+del Engineering cybernetics.out 
+del Engineering cybernetics.log
+del Engineering cybernetics.thm
+del Engineering cybernetics.toc
+del Engineering cybernetics.run.xml
+del Engineering cybernetics.blg
+del Engineering cybernetics.idx
+del Engineering cybernetics.synctex.gz
